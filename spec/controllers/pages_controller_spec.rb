a88dec10300af569c871bd20304a8e5a2d456f0e
Chris Blättler <chris.blaettler@gmail.com>
2014-07-16 08:53:25 +0200

    misc changes trying to fix have selector issue

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,8 +8,21 @@ RSpec.describe PagesController, :type => :controller do
       get 'home'
       expect(response).to be_success
     end
-  end
 
+# couldn't get this test running :(
+#
+   it "should have the right title" do
+#
+        get 'home'
+
+#response.should have_selector("title", :content => "Ruby")
+#expect(response).to have_selector("title", :content => "Ruby")
+#
+#expect(response).to have_title('Ruby')
+expect(response).to have_selector('title', :content => "Ruby")
+    end
+
+end
 
   describe "GET 'contact'" do
     it "returns http success" do
@@ -26,9 +39,4 @@ describe "GET 'about'" do
 end
 
 
-it "should have the right title" do
-    get 'home'
-    expect(response).to have_selector("title",
-    :conent => "Ruby on Rails Tutorial Sample App | Home")
-end
 end
